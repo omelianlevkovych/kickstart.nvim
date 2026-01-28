fb34372e89419fe5a9a58d2d031cafa7876fc1c5
Omelian Levkovych <omelianai@gmail.com>
2026-01-28 21:55:57 +0100

    add markdown extension and :Md allias

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,14 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  {
+    'iamcco/markdown-preview.nvim',
+    build = 'cd app && npm install',
+    ft = 'markdown',
+    cmd = { 'MarkdownPreview', 'Md' },
+    config = function()
+      vim.api.nvim_create_user_command('Md', 'MarkdownPreview', {})
+    end,
+  },
+}
