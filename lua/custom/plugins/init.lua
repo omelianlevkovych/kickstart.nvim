60887b325dbc6f35e16b99eb7a8e8a2922acc55b
Omelian Levkovych <ole@templafy.com>
2026-02-02 20:55:53 +0100

    add roslyn

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,6 +2,20 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
+-- Vertical line at 80 and 120 characters (line length guide)
+vim.opt.colorcolumn = '80,120'
+vim.api.nvim_set_hl(0, 'ColorColumn', { bg = '#2a2a2a' })
+
+-- Double-tab for fuzzy file search (from git root or cwd)
+vim.keymap.set('n', '<Tab><Tab>', function()
+  local git_root = vim.fn.systemlist('git rev-parse --show-toplevel')[1]
+  if vim.v.shell_error == 0 and git_root then
+    require('telescope.builtin').find_files({ cwd = git_root })
+  else
+    require('telescope.builtin').find_files()
+  end
+end, { desc = 'Fuzzy find files (project root)' })
+
 return {
   {
     'iamcco/markdown-preview.nvim',
@@ -10,6 +24,30 @@ return {
     cmd = { 'MarkdownPreview', 'Md' },
     config = function()
       vim.api.nvim_create_user_command('Md', 'MarkdownPreview', {})
+    end,
+  },
+  {
+    'seblj/roslyn.nvim',
+    ft = 'cs',
+    cmd = 'Roslyn',
+    opts = {
+      filewatching = 'roslyn',
+      choose_target = function(targets)
+        return targets[1]
+      end,
+    },
+    init = function()
+      -- Enable decompilation and external source navigation
+      vim.lsp.config('roslyn', {
+        settings = {
+          ['csharp|symbol_search'] = {
+            dotnet_search_reference_assemblies = true,
+          },
+          ['navigation'] = {
+            dotnet_enable_decompilation_support = true,
+          },
+        },
+      })
     end,
   },
 }
